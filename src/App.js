a96def4fa33e151706dce52f16ee9dfef0f8076b
suochantsao <suochantsao@gmail.com>
2021-09-03 16:40:05 +0800

    style settings

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
       <p>This is my first React App!</p>
+      <p className="test">HELLO WORLD</p>
     </div>
   );
 }
