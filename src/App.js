ef6e7a310bb91e3e2b1bf7e53f1c79916883bc36
suochantsao <suochantsao@gmail.com>
2021-09-04 19:47:06 +0800

    first react function component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+// components
+import PersonOne from "./components/PersonOne";
+
 function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
       <p>This is my first React App!</p>
       <p className="test">HELLO WORLD</p>
+      <PersonOne />
     </div>
   );
 }
---
+++ src/components/PersonOne.js
@@ -0,0 +1,10 @@
+function PersonOne() {
+  return (
+    <div className="person">
+      <h1>Bill</h1>
+      <p>Your Age: 26</p>
+    </div>
+  );
+}
+
+export default PersonOne;
