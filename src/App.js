a9cb3269d382a78fae86f5cb19711a9a48e5424a
suochantsao <suochantsao@gmail.com>
2021-09-25 22:28:58 +0800

    lifecycle Testing component init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import logo from "./logo.svg";
+import { Component } from "react";
 import "./App.css";
+// import logo from "./logo.svg";
 
 // components
-import PersonOne from "./components/PersonOne";
-import Test from "./components/Test";
+// import PersonOne from "./components/PersonOne";
+import Timer from "./components/Timer";
 
-function App() {
-  return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>This is my first React App!</p>
-      <PersonOne>28</PersonOne>
-      <Test></Test>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <p>This is my first React App!</p>
+//       <PersonOne>28</PersonOne>
+//       <Test></Test>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timerOn: false,
+    };
+    this.toggleTimer = this.toggleTimer.bind(this);
+  }
+
+  toggleTimer() {
+    this.setState({
+      timerOn: !this.state.timerOn,
+    });
+  }
+
+  render() {
+    const buttonText = this.state.timerOn ? "停止timer" : "開啟timer";
+    return (
+      <div>
+        <button onClick={this.toggleTimer}>{buttonText}</button>
+        {this.state.timerOn && <Timer />}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Timer.js
@@ -0,0 +1,35 @@
+// class Test extends Component {
+//   constructor(props) {
+//     super(props);
+//     console.log(this.props);
+
+//     this.state = { name: "Angela" };
+//   }
+
+//   render() {
+//     return <h1>{this.state.name}</h1>;
+//   }
+// }
+
+import { Component } from "react";
+
+class Timer extends Component {
+  // Mounting 生命週期的方法
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.timer = window.setInterval(() => {
+      console.log("hello world");
+    }, 1000);
+  }
+  // UnMounting 生命週期的方法
+  componentWillUnmount() {
+    window.clearInterval(this.timer);
+    console.log("componentWillUnmount");
+  }
+  // Class Component 必備的 render() 方法
+  render() {
+    return <div>timer is running</div>;
+  }
+}
+
+export default Timer;
