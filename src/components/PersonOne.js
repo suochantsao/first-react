472534efaed3dc6da1626c6ff1cde8dd13355ccd
suochantsao <suochantsao@gmail.com>
2021-09-29 13:26:40 +0800

    Hooks - useEffect added

--- src/components/PersonOne.js
+++ src/components/PersonOne.js
@@ -1,13 +1,16 @@
-import { Fragment } from "react";
-import { useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 
 function PersonOne() {
   const [test] = useState(100);
+  const [test2] = useEffect();
 
   return (
     <Fragment>
       <h1>Bill</h1>
-      <p>Your Age: {test}</p>
+      <p>
+        Your Age: {test}
+        {test2}
+      </p>
     </Fragment>
   );
 }
