f94269c2d8ae8cef074a56e7262cb1178e199bfa
suochantsao <suochantsao@gmail.com>
2021-10-04 05:46:09 +0800

    react-router-dom installed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import "./style/css/index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter, Switch } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <App />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
